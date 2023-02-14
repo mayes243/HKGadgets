3d23b8b4b3824838ee944736a112bd6100ddf5a0
mayeskhatri243 <mayeskhatri243@gmail.com>
2023-02-14 10:04:50 +0530

    configuration for images

--- next.config.js
+++ next.config.js
@@ -1,15 +1,15 @@
 const path = require("path");
 
 module.exports = {
-	sassOptions: {
-		includePaths: [path.join(__dirname, "styles")],
-	},
-	eslint: { ignoreDuringBuilds: true },
-	env: {
-		JWT_SECRET: "zxcvbnmasdfghjklpoiuytrewqasdfghmnbvcxzasdfghjloiuytreqsd",
-		CLOUDINARY_URL: "https://api.cloudinary.com/v1_1/dev-empty/image/upload",
-		CLOUD_NAME: "dev-empty",
-		UPLOAD_PRESET: "vikings",
-	},
-	trailingSlash: true,
+  sassOptions: {
+    includePaths: [path.join(__dirname, "styles")],
+  },
+  eslint: { ignoreDuringBuilds: true },
+  env: {
+    JWT_SECRET: "zxcvbnmasdfghjklpoiuytrewqasdfghmnbvcxzasdfghjloiuytreqsd",
+    CLOUDINARY_URL: "https://api.cloudinary.com/v1_1/weburetec/image/upload",
+    CLOUD_NAME: "weburetec",
+    UPLOAD_PRESET: "dtdqwfix",
+  },
+  trailingSlash: true,
 };
